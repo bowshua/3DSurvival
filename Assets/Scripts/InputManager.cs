8a1bf20c1cb3c66ac673e2a1c32019e4e1750311
bowshua <joshwarthunder@gmail.com>
2023-07-13 20:53:42 -0500

    your message here

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Animator animator;
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+
+    private PlayerMotor motor;
+    private PlayerLook look;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+        animator = GetComponent<Animator>();
+
+        onFoot.Jump.performed += ctx => motor.Jump();
+
+       
+        onFoot.Crouch.performed += ctx => motor.Crouch();
+        onFoot.Sprint.performed += ctx => motor.Sprint();
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //tell the playermotor to move using our movement action
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>()); 
+    }
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
